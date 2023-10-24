0810dbf6cdf2b13768392f522a69d79f385a4940
samuel <samuel.petersson@udla.edu.ec>
2023-10-24 15:19:29 -0500

    por fin el codigito esta terminado pero no pude hacer casi ningun comit y casi me muero pero por fin se me hiso uno despues de 4 intentos en

--- trabajo_en_clase_semana_4.c
+++ trabajo_en_clase_semana_4.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 
+//aqui elimine las anteriores definiciones que meti esto me costo pero ya nada
 int es_primo(int numero) {
-int num;
-int num2
-printf "escriba el numero que escanearemos y verificaremos si es primo y si es par"
+    if (numero <= 1) {
+        return 0;//con esto evito que me pongan numeros negativos o un 1 ya que tecnicamente es primo pero mejor no lo escribimos ya que si 1 fuera primo nungun numero que no sea decimal lo seria.
+    }
+    for (int i = 2; i * i <= numero; i++) {
+        if (numero % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
 
+int main() {
+    int n, numero, pares = 0, primos = 0;
+
+    printf("Ingrese la cantidad de números enteros que desees meter: ");//aqui digo cuantos numeros deseo meter como 
+    scanf("%d", &n);
+
+    printf("Ingrese los números naturales separados por espacios:\n");
+//gracias al for puedo saber con un par de simples codigos cuantos 
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &numero);
+
+        if (numero % 2 == 0) {
+            pares++;
+        }
+        if (es_primo(numero)) {
+            primos++;
+        }
+    }
+
+    float porcentaje_pares = (float)pares / n * 100;
+    float porcentaje_primos = (float)primos / n * 100; //el float basicamente me da escribiendo en porcentajes los numeros pares y primos que deseo meter
+
+    printf("Pares: %.2f%%, Primos: %.2f%%\n", porcentaje_pares, porcentaje_primos);
+
+    return 0;
 }
